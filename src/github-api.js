90a313c25b19ce6640933463d7e0df8277fc1d61
Lewis Nakao <lewdev@gmail.com>
2023-02-26 20:21:54 -1000

    Add gist get methods
    Token not required. Shows all gists

--- src/github-api.js
+++ src/github-api.js
@@ -13,6 +13,9 @@ const MyCrypt = (() => {
   };
 })();
 
+const SET_TOKEN_BTN = label => (
+  `<button class="btn btn-light btn-sm" onclick="GithubApi.showTokenPrompt()" title="Set Github Token">🎟${label ? " " + label : ""}</button>`
+);
 const HOW_TO_GEN_TOKEN =
 `<div id="tokenInstructions" class="p-3 bg-light rounded border border-secondary"
   style="position:fixed; top:10%; left:20%; width: 60%;">
@@ -43,16 +46,23 @@ const GithubApi = (() => {
 
   let username, tokenStr, token = null;
 
+  const handleError = error => {
+    console.error(error);
+    if (err) err.innerHTML = error;
+    if ((error + "").includes("Unauthorized")) showTokenPrompt();
+  };
+
   const postFetch = fetchPromise => fetchPromise
-    .then(r => r.ok ? r.json() : Promise.reject(`ERROR: Status ${r.status} ${r.statusText}`))
-    .catch(error => {
-      console.error(error);
-      if (err) err.innerHTML = error;
-      if (error.endsWith("Unauthorized")) showTokenPrompt();
+    .then(r => {
+      if (r.ok) return r.json();
+      const isUnauthorized = r.statusText === "Unauthorized";
+      Promise.reject(`ERROR: Status ${r.status} ${r.statusText}${isUnauthorized ? ` (invalid token) ${SET_TOKEN_BTN("Set Token")}` : ""}`)
+      return r;
     })
+    .catch(handleError)
   ;
 
-  const get = path => postFetch(fetch(`${API_URL}${path}`,{headers: {Authorization: `token ${token}`}}));
+  const get = (path, header = {}) => postFetch(fetch(`${API_URL}${path}`,{headers: {Authorization: token ? `token ${token}` : "", ...header}}));
 
   const patch = (url, body) => postFetch(fetch(url, {
     method: 'PATCH',
@@ -70,14 +80,25 @@ const GithubApi = (() => {
     else document.body.innerHTML += show ? HOW_TO_GEN_TOKEN : "";
   };
 
+  const formatGistData = data => (
+    (data || []).map(({ id, url, description, public, created_at, updated_at, files }) => ({
+      id, url, description, public, created_at, updated_at, 
+      files: Object.keys(files).map(key => {
+        const { filename, raw_url, size, content } = files[key];
+        return { filename, raw_url, size, content };
+      })
+    }))
+  );
+
   return {
     init: loadDataArg => new Promise((res, rej) => {
       tokenStr = localStorage.getItem(STORAGE_KEY);
-      if (!tokenStr) { rej(); return; }
       loadData = loadDataArg;
+      if (!tokenStr) { if (loadData) loadData(); res(); return; }
       token = MyCrypt.decrypt(SALT, tokenStr);
-      GithubApi.getUser().then(_ => {if (loadDataArg) loadDataArg(); res();});
+      GithubApi.getUser().then(_ => {if (loadData) loadData(); res();});
     }),
+    handleError,
     showTokenPrompt,
     hideTokenPrompt: () => showTokenPrompt(false),
     setToken: str => {
@@ -94,22 +115,19 @@ const GithubApi = (() => {
       username = data ? data.login : "";
       return data;
     }),
-    getGists: () => get(`/users/${username}/gists?per_page=1000`)
-      .then((data = []) => (
-        (data || []).map(({ id, url, description, public, created_at, updated_at, files }) => ({
-          id, url, description, public, created_at, updated_at, 
-          files: Object.keys(files).map(key => {
-            const { filename, raw_url } = files[key];
-            return { filename, raw_url };
-          })
-        }))
-      ))
-    ,
+    getGists: () => get(`/gists?per_page=1000`).then(formatGistData),
+    getGistById: gistId => get(`/gists/${gistId}`).then(r => formatGistData([r])[0]),
+    getGistsByUser: user => get(`/users/${user}/gists?per_page=1000`).then(formatGistData),
     saveGist: (gistId, body) => patch(`${API_URL}/gists/${gistId}`, body),
     saveFile: (gistId, filename, content) => {
       const files = {};
       files[filename] = { content };
-      return GithubApi.saveGist(gistId, {files});
+      return GithubApi.saveGist(gistId, {files}).then(result => {
+        if (result.message === "Bad credentials") {
+          Promise.reject("Invalid Github Token with incorrect permissions.");
+        }
+        return result;
+      });
     },
     // getRepos: cb => get(`/user/repos?per_page=100&type=public`)
     //   .then(data => data.map(d => (
